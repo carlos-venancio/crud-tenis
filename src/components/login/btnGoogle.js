55170197e526f439b385d5ea471e4384d2a6173c
Arnold Silva <124202278+ArnoldSilva@users.noreply.github.com>
2023-11-27 14:42:31 -0300

    Delete src directory

--- src/components/login/btnGoogle.js
+++
@@ -1,9 +0,0 @@
-
-export default function btnGoogle(){
-    return (
-        <button className="p-5 items-center flex mt-7 justify-center bg-white-800  text-black  max-sm:max-w-xs w-60 h-8 rounded-lg hover:scale-105 border border-gray-300">
-           <img src="/logoGoogle.png" className="h-7 mr-4"/>
-            Sign in with Google 
-        </button>
-    )
-}
