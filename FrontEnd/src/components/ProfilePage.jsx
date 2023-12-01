2a9d4c7404ca9e39d93e120b467ecda09c528d4f
Arnold Silva <124202278+ArnoldSilva@users.noreply.github.com>
2023-12-01 11:38:28 -0300

    Add files via upload

---
+++ FrontEnd/src/components/ProfilePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div className="items-center bg-white flex flex-col px-20 max-md:px-5">
+     
+
+     atesateasfgwdg 
+
+    </div>
+  );
+}
+
+
+export default Profile;
