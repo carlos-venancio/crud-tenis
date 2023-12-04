b4331c953d36c7c2a587f8a1cfa2a2579e3148ac
Arthur <arthurferreira1417@gmail.com>
2023-12-04 00:36:32 -0300

    finalizei a home e a responsividade e iniciei a dos produtos

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import FullScreen from "./components/Full_Screen"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+// reaproveitando a estrutura
+import { Outlet } from "react-router-dom";
+import Products from "./components/products"
+
+
+
+function App() {
+ 
+  return (
+    <div className="">
+    <Header/>
+    
+    <Outlet/>
+
+    <Products/>
+    
+    <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
