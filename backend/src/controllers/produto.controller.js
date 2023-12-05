d6c1d23d17a6ac6da907b793e1ee429fcb248a93
erickfranc <132010888+erickfranc@users.noreply.github.com>
2023-12-05 17:17:36 -0300

    Delete backend/src directory

--- backend/src/controllers/produto.controller.js
+++
@@ -1,5 +0,0 @@
-import repositories from '../repositories/produto.repositories.js'
-
-export async function cadastrarProduto(req,res){
-    repositories.cadastrarProduto()
-} 
--- backend/src/routes/produto.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { cadastrarProduto } from "../controllers/produto.controller.js";
-
-const routes = Router();
-
-routes.post('/', cadastrarProduto)
-
-export default routes
