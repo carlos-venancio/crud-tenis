f85e7cd48a9426992b75ec461afaed7b14e753e8
erickfranc <132010888+erickfranc@users.noreply.github.com>
2023-12-08 02:04:36 -0300

    Update search.controller.js

--- backend/src/controllers/search.controller.js
+++ backend/src/controllers/search.controller.js
@@ -4,7 +4,6 @@ import { pesquisarNoBanco } from "../repositories/search.repository.js";
 export const realizarPesquisa = async (req, res) => {
   try {
     const termoPesquisa = req.query.q;
-    const resultadosPesquisa = await pesquisarNoBanco(termoPesquisa);
 
     res.status(200).json({
       message: "item encontrado com sucesso",
