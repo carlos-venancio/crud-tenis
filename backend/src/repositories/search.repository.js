55e9c15204e075cadc33be9b8664f66814226a8d
erickfranc <132010888+erickfranc@users.noreply.github.com>
2023-12-08 08:33:00 -0300

    Update search.repository.js

--- backend/src/repositories/search.repository.js
+++ backend/src/repositories/search.repository.js
@@ -1,24 +1,16 @@
 import produto from "../models/Produto.js";
 
-const { q } = req.query;
-
- if (!q) {
-    return res.status(400).json({ error: 'a busca não pode estar vazia.' });
- }
-
- try {
-    const products = await produto.find({
-      $or: [
-        { modelo: { $regex: q, $options: 'i' } },
-        { fk_marcanome: { $regex: q, $options: 'i' } },
-      ],
+export const pesquisarNoBanco = async (termoPesquisa) => {
+  try {
+    const resultados = await produto.find({
+        $or: [
+            {modelo: { $regex: new RegExp(termoPesquisa, "i") }},
+            {fk_marcanome: { $regex: new RegExp(termoPesquisa, "i") } }
+        ]
     });
 
-    if (!products.length) {
-      return res.status(404).json({ error: 'nenhum produto encontrado.' });
-    }
-
-    return res.json(products);
- } catch (error) {
-    return res.status(500).json({ error: 'erro ao pesquisar produtos.' });
- };
+    return resultados;
+  } catch (error) {
+    throw new Error("erro ao realizar a pesquisa");
+  }
+};
