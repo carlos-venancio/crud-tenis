8432b03accc2c1e910962ce167e0d15ab17c6cc7
erickfranc <132010888+erickfranc@users.noreply.github.com>
2023-12-11 15:59:04 -0300

    Add files via upload

--- backend/src/controllers/search.controller.js
+++ backend/src/controllers/search.controller.js
@@ -1,16 +1,18 @@
-import { pesquisarNoBanco } from "../repositories/search.repository.js";
-
+import pesquisarNoBanco from '../repositories/search.repository.js';
 
 export const realizarPesquisa = async (req, res) => {
   try {
     const termoPesquisa = req.query.q;
 
+    const resultados = await pesquisarNoBanco(termoPesquisa);
+
     res.status(200).json({
-      message: "item encontrado com sucesso",
+      message: "Itens encontrados com sucesso",
+      resultados,
     });
   } catch (error) {
     res.status(500).json({
-      message: "erro ao achar item",
+      message: "Erro ao realizar a pesquisa",
       error: error.message,
     });
   }
